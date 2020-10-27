39992d3ee9a4df8e196d3dc49c4f017e9820c6a3
Rachana Devi <rachanadevi18@gmail.com>
2020-10-27 15:52:49 +0530

    [Rachana] Add. concatenation to see if "After error" and "Completed" is printed if exception is thrown

--- src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
+++ src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
@@ -10,10 +10,12 @@ class FluxAndMonoTest {
         Flux<String> stringFlux = Flux.just("Spring", "Spring boot", "Reactive spring")
                 //uncomment this to find onError() getting executed
 //                .concatWith(Flux.error(new RuntimeException("Exception occurred!")))
+                .concatWith(Flux.just("After error"))
                 .log();
 
         stringFlux
                 .subscribe(System.out::println,
-                        (ex) -> System.err.println("Exception is" + ex));
+                        (ex) -> System.err.println("Exception is" + ex),
+                        ()-> System.out.println("Completed"));
     }
 }
