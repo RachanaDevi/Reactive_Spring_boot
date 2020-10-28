5629cc264258734477f44cbd21a2bbb05688a076
Rachana Devi <rachanadevi18@gmail.com>
2020-10-28 16:57:30 +0530

    [Rachana] Test.  For Functional routes for flux and mono Learning. Annotations SpringBootTest and AutoConfigureWebTestClient

---
+++ src/test/java/com/learnreactivespring/handler/SampleHandlerFunctionTest.java
@@ -0,0 +1,51 @@
+package com.learnreactivespring.handler;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+class SampleHandlerFunctionTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    public void shouldValidateResponseUsingStepVerifierForFunctionalFlux() {
+        Flux<Integer> integerFlux = webTestClient.get().uri("/functional/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(integerFlux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void shouldValidateFunctionalMono() {
+        Integer expectedValue = 1;
+        webTestClient.get().uri("/functional/mono")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Integer.class)
+                .consumeWith((response) -> assertThat(response.getResponseBody(), is(expectedValue)));
+    }
+}
