53622af0b1a111678d97082dbceeb41ff6acf752
Rachana Devi <rachanadevi18@gmail.com>
2020-10-28 13:27:41 +0530

    [Rachana] Test. ways to validate the Flux response

--- src/test/java/com/learnreactivespring/controller/FluxMonoControllerTest.java
+++ src/test/java/com/learnreactivespring/controller/FluxMonoControllerTest.java
@@ -7,23 +7,27 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 @ExtendWith(SpringExtension.class)
 @WebFluxTest
-@AutoConfigureWebTestClient(timeout="9000")
+@AutoConfigureWebTestClient(timeout = "9000")
 class FluxMonoControllerTest {
 
-    /*We need a non blocking client to tests
-      non blocking endpoint
-     */
     @Autowired
     WebTestClient webTestClient;
 
     @Test
-    public void fluxTestApproach() {
+    public void shouldValidateResponseUsingStepVerifier() {
         Flux<Integer> integerFlux = webTestClient.get().uri("/flux")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
@@ -33,7 +37,45 @@ class FluxMonoControllerTest {
 
         StepVerifier.create(integerFlux)
                 .expectSubscription()
-                .expectNext(5,6,7,8)
+                .expectNext(5, 6, 7, 8)
                 .verifyComplete();
+    }
+
+    @Test
+    public void shouldValidateResponseByHeaderAndBodyListSize() {
+        webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Integer.class)
+                .hasSize(4);
+    }
+
+    @Test
+    public void shouldValidateResponseByComparingResponseWithList() {
+        List<Integer> expectedIntegerList = Arrays.asList(5, 6, 7, 8);
+
+        EntityExchangeResult<List<Integer>> entityExchangeResult = webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Integer.class)
+                .returnResult();
+
+        assertThat(entityExchangeResult.getResponseBody(), is(expectedIntegerList));
+    }
+
+    @Test
+    void shouldValidateResponseUsingConsumeWithUsedFromExpectBodyList() {
+        List<Integer> expectedIntegerList = Arrays.asList(5, 6, 7, 8);
+
+        webTestClient
+                .get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Integer.class)
+                .consumeWith((response)->assertThat(response.getResponseBody(),is(expectedIntegerList)));
     }
 }
