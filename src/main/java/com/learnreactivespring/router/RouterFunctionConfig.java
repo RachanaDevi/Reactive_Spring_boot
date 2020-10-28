97d60ffffd5e6644fe7c76a4584e8f101519ae45
Rachana Devi <rachanadevi18@gmail.com>
2020-10-28 15:49:30 +0530

    [Rachana] Add. router and handlers for flux and mono

--- src/main/java/com/learnreactivespring/router/RouterFunctionConfig.java
+++ src/main/java/com/learnreactivespring/router/RouterFunctionConfig.java
@@ -17,7 +17,7 @@ public class RouterFunctionConfig {
     public RouterFunction<ServerResponse> route(SampleHandlerFunction sampleHandlerFunction) {
         return RouterFunctions
                 .route(GET("/functional/flux").and(accept(MediaType.APPLICATION_JSON)), sampleHandlerFunction::flux)
-                .andRoute(GET("/functional/mono").and(accept(MediaType.APPLICATION_JSON)),sampleHandlerFunction::mono);
+                .andRoute(GET("/functional/mono").and(accept(MediaType.APPLICATION_JSON)), sampleHandlerFunction::mono);
 
     }
 }
